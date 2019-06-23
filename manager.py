3e1fea7f1a22a1bc315ac8f0960a91cb37bf3b8b
2675694536 <52022141+2675694536@users.noreply.github.com>
2019-06-23 14:58:06 +0800

    Delete manager.py

--- manager.py
+++
@@ -1,8 +0,0 @@
-from flask_migrate import MigrateCommand
-from flask_script import Manager
-from app import creat_app
-app = creat_app('development')
-manage=Manager(app)
-manage.add_command('db',MigrateCommand)
-if __name__ == '__main__':
-    manage.run()
